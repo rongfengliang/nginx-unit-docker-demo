efcc23ed41e4af13fc173ddb91d2275d8cfa8301
dalongrong <1141591465@qq.com>
2018-05-11 16:34:57 +0800

    add golang support

--- app.go
+++ app.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"math"
 	"net/http"
 	"nginx/unit"
 )
-
-func term(ch chan float64, k float64) {
-	ch <- 4 * math.Pow(-1, k) / (2*k + 1)
-}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("appdemorong"))
